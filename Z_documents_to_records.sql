a3d1cf0d455bce8a02c8c5523d8a088fa434ed37
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-11-21 11:24:53 -0500

    Update Z_documents_to_records.sql
    This captures the file location, transaction ID, and the description for posterity. Needs to be cross-referenced to job/contract/work order in the future.

--- Z_documents_to_records.sql
+++ Z_documents_to_records.sql
@@ -1,9 +1,17 @@
-  -- Document location to Transaction_ID
+-- Document location to Transaction_ID
 SELECT
 	d.*
-	--,x.Transaction_ID
+	,x.Transaction_ID
+	--,x.*
+	,m.Transaction_Description
+	--m.*
 FROM
 	DI_IMAGE_MASTER_VIEW AS d
 	JOIN
-	DI_IMAGE_XREF_VIEW as x
+	DI_IMAGE_XREF_VIEW AS x
 		ON d.Document_ID = x.Document_ID
+	LEFT JOIN
+	DI_MASTER_MC AS m
+		ON x.Transaction_ID = m.Transaction_ID
+
+ORDER BY d.Document_ID DESC
