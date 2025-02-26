a3b368f5b66b2f66b5f355137faf20252a96cfaa
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2025-02-26 10:52:03 -0500

    Update Z_PBI_Spectrum_historical_service_contracts_costs_per_visit.sql
    Include aggregated costs.

--- Z_PBI_Spectrum_historical_service_contracts_costs_per_visit.sql
+++ Z_PBI_Spectrum_historical_service_contracts_costs_per_visit.sql
@@ -1,17 +1,27 @@
+---- Contract information
+/* Top-level contract information, contact information for the site and customer, sales rep information, margins, etc. */
 SELECT --TOP 100
-  CAST(c.WO_Number AS int) AS WO_Number,
-  LTRIM(RTRIM(c.Site_Id)) AS Site_Code,
-  LTRIM(RTRIM(c.Contract_Number)) AS Contract_Number,
-  c.Tran_Source,
-  c.Tran_Type,
-  SUM(c.Transaction_Amount) AS Amt,
-  c.GL_Date
-  --,c.*
+	LTRIM(RTRIM(s.Contract_Number)) AS Contract_Number,
+	LTRIM(RTRIM(s.Customer_Code)) AS Customer_Code,
+	LTRIM(RTRIM(s.Site_ID)) AS Site_Code,
+	s.Description AS Contract_Type,
+	s.Scheduled_Visit_Count AS Number_Visits,
+	s.Number_Of_Bill_Periods AS Number_Billings,
+	s.Begin_Date AS Contract_Start,
+	s.End_Date AS Contract_End,
+	s.Salesman,
+	s.Contract_Amount,
+	c.Tran_Type,
+	c.Cost
+	--,s.*
+	
 FROM
-  WO_COST_HISTORY_MC AS c
+	SC_CONTRACT_MC AS s
+	LEFT OUTER JOIN
+    (SELECT Contract_Number, Tran_Type, SUM(Amt) AS Cost
+	FROM Z_PBI_Spectrum_historical_service_contracts_costs_per_visits
+	GROUP BY Contract_Number, Tran_Type) AS c
+		ON LTRIM(RTRIM(s.Contract_Number)) = c.Contract_Number
+    
 WHERE
-  c.Company_Code = 'NA2' 
-  AND c.Contract_Number <> '' 
-  --AND LTRIM(RTRIM(c.WO_Number)) = '10279'
-GROUP BY c.Site_Id,c.Contract_Number,c.WO_Number,GL_Date,c.Tran_Source,C.Tran_Type
-ORDER BY CAST(c.WO_Number AS int) DESC
+	s.Company_Code = 'NA2'
